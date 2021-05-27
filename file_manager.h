07c9206e1f699c55747c5bd16049507571cdada5
BautistaXifro <bauxifro@gmail.com>
2021-05-27 14:31:04 -0300

    feature graphs

--- file_manager.h
+++ file_manager.h
@@ -18,6 +18,7 @@ class FileManager{
         explicit FileManager(const char* target_filepath,
          const char* index_filepath);
         FileManager(FileManager&& other);
+        FileManager(const FileManager& other) = delete;
         void fill(ProtectedMap* map);
         void fill(ProtectedQueue* queue);
         ~FileManager();
